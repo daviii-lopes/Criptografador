cf783c1c31b11c231314192fb0f58240bbfe22b9
Davi_Lopes <sou_puro_osso@proton.me>
2023-02-17 08:56:27 -0300

    Criado a primeira ideia de criptografia, mas com erros. O criptografador não tem reação quando a chave de descriptografia exige uma letra que passa do 'Z'.

--- main.rb
+++ main.rb
@@ -1,2 +1,12 @@
-def descriptografar frase, chave
+def criptografar palavra, chave
+  alfabeto = "a b c d e f g h i j k l m n o p q r s t u v w x y z ç".split " " 
+  array = palavra.split ""
+
+  for i in 0..array.size - 1 do
+    alfabeto_i = alfabeto.index array[i]
+    array[i] = alfabeto[alfabeto_i + chave] 
+  end
+  puts array
 end
+criptografar "abcdef", 3    
+
